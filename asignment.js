9ad2ff5df009652788d86bffc0634784ce810b6a
unknown <shifarhossain330gmail.com>
2023-05-31 12:21:22 +0600

    first commit

---
+++ asignment.js
@@ -0,0 +1,56 @@
+0-variable ki?
+answ=variable holo kono ekta bostu ke proyojon onojaeii bebohar,,poriborton,,kora..
+
+1-variable kivabe likhte hoy?
+answ=var
+
+2-String type variable ki? and kivabe likhte hoy?
+answ=je variable shudho word k chine thake take string type variable bole..
+
+var strtype = 'world';
+console.log(strtype);
+
+3-Number type variable ki? kivabe likhte hoy?
+answ=je variable shudho 012----9 shongkhake niye kaj kore take number type variable.
+var numtype = 234;
+console.log(numtype);
+
+4-boolean type variable ki? kivabe likhte hoy?
+answ=boolean type variable shudho true or false nirdharon kore..
+var booltype = true;
+console.log(booltype);
+
+5-toUppercase or toLowercase kivabe likhte hoy?
+ex:
+var booltype = true;
+console.log(booltype);
+
+6-Math.abs er bevohar likho?
+ex:
+var num = -20;
+var sum = math.abs(num);
+console.log(sum);
+
+7-math.round er bevohar likho?
+ex:
+var num = 3.5;
+var sum = math.round(num);
+console.log(sum);
+
+8-math.ceil er bevohar likho?
+ex:
+var num = .0002;
+var sum = math.ceil(num);
+console.log(sum);
+
+9-math.floor er bevohar likho?
+ex:
+var num = .5;
+var sum = math.floor(num);
+console.log(sum);
+
+10-math.random er bevohar likho?
+ex:
+var sum = math.random();
+console.log(sum);
+
---
+++ main.js
@@ -0,0 +1,8 @@
+0-variable ki?
+answ=variable holo kono ekta bostu ke proyojon onojaeii bebohar,,poriborton,,kora..
+
+1-variable kivabe likhte hoy?
+answ=var
+
+2-String type variable ki? and kivabe likhte hoy?
+answ=je variable shudho word 
